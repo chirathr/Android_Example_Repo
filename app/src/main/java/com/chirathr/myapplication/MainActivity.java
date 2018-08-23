132a9bab8f944e9e4071046be29c30185f526112
Chirath R <chirath.02@gmail.com>
2018-08-23 15:16:50 +0530

    Added Recycler view to the layout

---
+++ app/src/main/java/com/chirathr/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.chirathr.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        List<DataModel> dataModelList = new ArrayList<>();
+        for (int i = 1; i <= 20; ++i) {
+            dataModelList.add(new DataModel(i));
+        }
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter and pass in our data model list
+        mAdapter = new MyAdapter(dataModelList, this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
