4fb815a9aca0362f55ac05c24ee3ccfcbe5a5919
Chirath R <chirath.02@gmail.com>
2018-09-05 19:20:59 +0530

    Notifcation tutorial

--- app/src/main/java/com/chirathr/myapplication/MainActivity.java
+++ app/src/main/java/com/chirathr/myapplication/MainActivity.java
@@ -1,9 +1,20 @@
 package com.chirathr.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -14,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private static final String CHANNEL_ID = "simple_notification_channel";
+    private static final int PENDING_INTENT_ID = 1002;
+    private static final int NOTIFICATION_ID = 1002;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +53,60 @@ public class MainActivity extends AppCompatActivity {
         // specify an adapter and pass in our data model list
         mAdapter = new MyAdapter(dataModelList, this);
         mRecyclerView.setAdapter(mAdapter);
+
+        // create a notification
+        createSimpleNotification();
+    }
+
+    public void createSimpleNotification() {
+        // Get the notification manager system service
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Create notification channels for Android O and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            // Register the channel with the system; you can't change the importance or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // load the image for large icon
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.profile_pic);
+
+        // create the pending intent that will open the app when clicked on the notification
+        Intent startActivityIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                PENDING_INTENT_ID,
+                startActivityIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        // Build the notification
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setLargeIcon(largeIcon)
+                        .setContentTitle("Hi there!")
+                        .setContentText("Message from Chirath")
+                        .setDefaults(Notification.DEFAULT_VIBRATE)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+        // Priority for devices other than oreo and below
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+
+        if (notificationManager != null) {
+            notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+        }
+
     }
 }
